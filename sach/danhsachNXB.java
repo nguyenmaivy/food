be73154533b768a006e630580af6d6108760deab
sÒwÓs <115623421+NhatNam15151515@users.noreply.github.com>
2023-11-29 17:53:05 +0700

    upload-29/11/2023

---
+++ sach/danhsachNXB.java
@@ -0,0 +1,464 @@
+package sach;
+
+import java.io.*;
+import java.util.*;
+
+public class danhsachNXB {
+	
+	public static Scanner sc = new Scanner(System.in);
+	private static int soluong = 0;
+	private static newNXB NXB[];
+
+	public static int getSoluong() {
+		return soluong;
+	}
+
+	public static void setSoluong(int sl) {
+		soluong = sl;
+	}
+
+	// nhập thông tin
+	public static void nhapdanhsach() {
+		while (soluong == 0) {
+			
+			System.out.print("Nhap so luong NXB ");
+			String sl = sc.nextLine();
+			
+			if (sl.matches("\\d+"))
+				soluong = Integer.parseInt(sl);
+			else
+				soluong = 0;
+			
+		}
+		
+		NXB = new newNXB[soluong];
+		
+		for (int i = 0; i < NXB.length; i++) {
+			NXB[i] = new newNXB();
+			
+			System.out.println("Nhap thong tin NXB");
+			
+			NXB[i].nhap();
+			
+		}
+	}
+	//đọc file
+	public static void docfile(String filename) {
+		try {
+			
+			FileReader readfile = new FileReader(filename);
+			BufferedReader br = new BufferedReader(readfile);
+			String line;
+			int i;
+			
+			while ((line = br.readLine()) != null) {
+				
+				String text[] = line.split("#");
+				
+				if (NXB == null)
+					NXB = new newNXB[1];
+				else
+					NXB = Arrays.copyOf(NXB, NXB.length + 1);
+				
+				i = NXB.length - 1;
+				
+				if (text.length == 3 && Trunglap(text[0]) == null && text[1].matches("\\d+")) {
+					if (NXB[i] == null)
+						NXB[i] = new newNXB(text[0], text[1], text[2], 1);
+					// NXB[i].capnhatNXB(text[0], text[1], text[2], 1);
+					i++;
+					
+				} else
+					NXB = Arrays.copyOf(NXB, NXB.length - 1);
+				
+			}
+			
+			soluong = NXB.length;
+			br.close();
+			readfile.close();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+	}
+	//ghi file
+	public static void ghifile(String filename) {
+		try {
+			
+			FileWriter writefile = new FileWriter(filename);
+			FileWriter bin = new FileWriter("D:\\java\\java_project\\src\\sach\\NXB_recyclebin.txt");
+			int i = 0;
+			
+			if (NXB == null) {
+				System.out.println("Danh sach rong");
+				writefile.close();
+				bin.close();
+				return;
+			}
+			
+			while (i < NXB.length && NXB[i] != null) {
+				if (NXB[i].trangthai == 1)
+					writefile.write(NXB[i].getTenNXB() + "#" + NXB[i].getsdtNXB() + "#" + NXB[i].getDcNXB() + "\n");
+				else
+					bin.write(NXB[i].getTenNXB() + "#" + NXB[i].getsdtNXB() + "#" + NXB[i].getDcNXB() + "\n");
+				i++;
+			}
+			
+			writefile.close();
+			bin.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// tìm kiếm thông tin bị trùng lặp
+	// trùng lặp được cho phép
+	public static newNXB Trunglap(String tenNXB) {
+		if (NXB == null || NXB[0] == null)
+			return null;
+		
+		for (int i = 0; i < NXB.length; i++)
+			if (NXB[i] != null)
+				if (NXB[i].getTenNXB() != null && NXB[i].getTenNXB().equals(tenNXB))
+					return NXB[i];
+		
+		return null;
+		
+	}
+
+	// xuất thông tin
+	public static void xuatdanhsach() {
+		if (NXB.length == 0) {
+			System.out.println("Khong co NXB nao");
+			return;
+		}
+		
+		for (int i = 0; i < NXB.length; i++) {
+			if (NXB[i] != null && NXB[i].trangthai == 1) {
+				System.out.println("");
+				System.out.println(NXB[i].xuat());
+			}
+			
+		}
+	}
+	//sắp xếp theo tên
+	public static newNXB[] Sapxep_ten() {
+		if (NXB == null || NXB.length <= 1)
+			return NXB;
+
+		newNXB NXB_sapxep[] = Arrays.copyOf(NXB, NXB.length);
+		newNXB tmp;
+
+		for (int i = 0; i < NXB_sapxep.length; i++) {
+			for (int j = 0; j < NXB_sapxep.length - i - 1; j++) {
+				String tenNXB1 = NXB_sapxep[j].getTenNXB().substring(0,1).toLowerCase();
+				String tenNXB2 = NXB_sapxep[j + 1].getTenNXB().substring(0,1).toLowerCase();
+
+				if (tenNXB1.compareTo(tenNXB2) > 0) {
+					tmp = NXB_sapxep[j];
+					NXB_sapxep[j] = NXB_sapxep[j + 1];
+					NXB_sapxep[j + 1] = tmp;
+				}
+			}
+		}
+		return NXB_sapxep;
+	}
+
+	public static void SapxepTheoTen() {
+		if (NXB == null)
+			return;
+		
+		newNXB NXB_sapxep[] = Arrays.copyOf(Sapxep_ten(), Sapxep_ten().length);
+		
+		for (int i = 0; i < NXB_sapxep.length; i++) {
+			if (NXB_sapxep[i] != null && NXB_sapxep[i].trangthai == 1) {
+				System.out.println("");
+				System.out.println(NXB_sapxep[i].xuat());
+			}
+
+		}
+	}
+
+	// thêm thông tin đối tượng
+	public static newNXB Them() {
+		int i;
+		
+		if (NXB == null)
+			NXB = new newNXB[1];
+		else
+			NXB = Arrays.copyOf(NXB, NXB.length + 1);
+		
+		i = NXB.length - 1;
+		NXB[i] = new newNXB();
+		
+		System.out.println("nhap thong tin NXB");
+		
+		NXB[i].nhap();
+		soluong = NXB.length;
+		
+		return NXB[i];
+		
+	}
+
+	public static void ThemNXB() {
+		int so_luong_can_them = -1;
+		
+		while (so_luong_can_them == -1) {
+			
+			System.out.print("So Luong NXB can them: ");
+			String sl = sc.nextLine();
+			
+			if (sl.matches("\\d+")) {
+				so_luong_can_them = Integer.parseInt(sl);
+				
+				if (so_luong_can_them == 0) {
+					if (soluong == 0)
+						System.out.println("Danh sach rong");
+					else
+						System.out.println("Khong them sach");
+					return;
+				}
+				
+				while (so_luong_can_them != 0) {
+					Them();
+					so_luong_can_them--;
+				}
+				
+			} else
+				so_luong_can_them = -1;
+		}
+	}
+
+	// tìm kiếm thông tin
+	public static int Timkiem(String tenNXB) {
+		for (int i = 0; i < NXB.length; i++) {
+			if (NXB[i].trangthai == 1 && NXB[i].getTenNXB().equals(tenNXB))
+				return i;
+		}
+		
+		return -1;
+		
+	}
+
+	public static void TimkiemNXB() {
+		if (NXB == null) {
+			System.out.println("Danh sach rong");
+			return;
+		}
+		
+		System.out.print("Nhap ten NXB can tim: ");
+		String tenNXB = sc.nextLine();
+		
+		int i = Timkiem(tenNXB);
+		
+		while (i == -1) {
+			
+			System.out.println("Khong tim thay");
+			System.out.println("1.Tiep tuc tim kiem");
+			System.out.println("2.Thoat tim kiem");
+			
+			int luachon = Integer.parseInt(sc.nextLine());
+			
+			if (luachon == 1)
+				TimkiemNXB();
+			else
+				return;
+			
+		}
+		
+		System.out.println(NXB[i].xuat());
+	}
+
+	// xóa thông tin(xóa giả => ẩn thông tin)
+	public static void Xoa() {
+		System.out.print("Nhap ten NXB can xoa: ");
+		String tenNXB = sc.nextLine();
+		
+		int i = Timkiem(tenNXB);
+		
+		if (i == -1) {
+			
+			System.out.println("Khong tim thay");
+			System.out.println("1.Tiep tuc tim kiem");
+			System.out.println("2.Thoat tim kiem");
+			
+			int luachon = Integer.parseInt(sc.nextLine());
+			
+			if (luachon == 1)
+				Xoa();
+			
+		} else {
+			NXB[i].trangthai = 0;
+			return;
+		}
+		
+	}
+
+	public static void XoaNXB() {
+		if (NXB == null) {
+			System.out.println("Danh sach rong");
+			return;
+		}
+		
+		int so_luong_can_xoa = -1;
+		
+		while (so_luong_can_xoa == -1) {
+			
+			System.out.print("Nhap so luong NXB can xoa thong tin (Khong vuot qua " + NXB.length + "): ");
+			String sl = sc.nextLine();
+			
+			if (sl.matches("\\d+")) {
+				so_luong_can_xoa = Integer.parseInt(sl);
+				
+				if (so_luong_can_xoa == 0) {
+					System.out.println("Khong xoa NXB");
+					return;
+				}
+				
+				if (so_luong_can_xoa > NXB.length)
+					return;
+				
+				while (so_luong_can_xoa != 0) {
+					Xoa();
+					so_luong_can_xoa--;
+				}
+				
+			} else
+				so_luong_can_xoa = -1;
+		}
+	}
+
+	// khôi phục thông tin đã xóa
+	public static newNXB Khoiphuc(String tenNXB_cankhoiphuc) {
+		int i;
+		
+		for (i = 0; i < NXB.length; i++)
+			if (NXB[i].getTenNXB() != null && NXB[i].getTenNXB().equals(tenNXB_cankhoiphuc) && NXB[i].trangthai == 0) {
+				NXB[i].trangthai = 1;
+				return NXB[i];
+			}
+		
+		return null;
+		
+	}
+
+	public static void KhoiphucNXB() {
+		if (NXB == null) {
+			System.out.println("Danh sach rong");
+			return;
+		}
+		
+		int soluong_NXB_cankhoiphuc = -1;
+		
+		while (soluong_NXB_cankhoiphuc == -1) {
+			
+			System.out.print("Nhap so luong NXB can khoi phuc thong tin (Khong vuot qua " + NXB.length + "): ");
+			String sl = sc.nextLine();
+			
+			if (sl.matches("\\d+")) {
+				soluong_NXB_cankhoiphuc = Integer.parseInt(sl);
+				
+				if (soluong_NXB_cankhoiphuc > NXB.length)
+					return;
+				
+				for (int i = 0; i < soluong_NXB_cankhoiphuc; i++) {
+					System.out.println("Nhap Ten NXB can khoi phuc: ");
+					String tenNXB_cankhoiphuc = sc.nextLine();
+					Khoiphuc(tenNXB_cankhoiphuc);
+					
+				}
+			} else
+				soluong_NXB_cankhoiphuc = -1;
+		}
+	}
+
+	// sửa thông tin
+	public static newNXB Sua(String tenNXB) {
+		int i = Timkiem(tenNXB);
+		
+		if (i == -1) {
+			
+			System.out.println("Khong tim thay");
+			System.out.println("1.Tiep tuc tim kiem");
+			System.out.println("2.Thoat tim kiem");
+			
+			int luachon = Integer.parseInt(sc.nextLine());
+			
+			switch (luachon) {
+
+			case 1:
+				System.out.print("Nhap Ten NXB can sua thong tin: ");
+				return Sua(sc.nextLine());
+			default:
+				return null;
+				
+			}
+			
+		}
+		
+		int so_luong_thong_tin_can_sua = -1;
+		
+		while (so_luong_thong_tin_can_sua == -1) {
+			
+			System.out.println("Ban muon sua bao nhieu thong tin cua NXB (toi da 3): ");
+			String sl = sc.nextLine();
+			
+			if (sl.matches("\\d+")) {
+				so_luong_thong_tin_can_sua = Integer.parseInt(sl);
+				
+				if (so_luong_thong_tin_can_sua > 3)
+					return null;
+				
+				while (so_luong_thong_tin_can_sua != 0) {
+					System.out.println("Sua thong tin cua NXB '" + NXB[i].getTenNXB() + "'");
+					NXB[i].suathongtin();
+					so_luong_thong_tin_can_sua--;
+				}
+				
+			} else
+				so_luong_thong_tin_can_sua = -1;
+		}
+		
+		return NXB[i];
+		
+	}
+
+	public static void SuaNXB() {
+		if (NXB == null) {
+			System.out.println("Danh sach rong");
+			return;
+		}
+		
+		int so_luong_can_sua = -1;
+		
+		while (so_luong_can_sua == -1) {
+			
+			System.out.print("Nhap so luong NXB can sua thong tin (Khong vuot qua " + NXB.length + "): ");
+			String sl = sc.nextLine();
+			
+			if (sl.matches("\\d+")) {
+				so_luong_can_sua = Integer.parseInt(sl);
+				
+				if (so_luong_can_sua == 0) {
+					System.out.println("Khong sua NXB");
+					return;
+				}
+				
+				if (so_luong_can_sua > NXB.length)
+					return;
+				
+				while (so_luong_can_sua != 0) {
+					System.out.print("Nhap ten NXB can sua thong tin: ");
+					String tenNXB = sc.nextLine();
+					Sua(tenNXB);
+					so_luong_can_sua--;
+					
+				}
+			} else
+				so_luong_can_sua = -1;
+		}
+	}
+}
---
+++ sach/newNXB.java
@@ -0,0 +1,254 @@
+package sach;
+
+public class newNXB extends NXB {
+
+	public newNXB() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public newNXB(String tenNXB, String dcNXB, String sdtNXB, int trangthai) {
+		super(tenNXB, dcNXB, sdtNXB, trangthai);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void nhap() {
+		while (this.getTenNXB() == null || this.getTenNXB().isEmpty()) {
+			
+			System.out.print("Nhap Ten NXB sach: ");
+			String newtenNXB = sc.nextLine();
+			
+			NXB new_NXB = danhsachNXB.Trunglap(newtenNXB);
+			
+			if (new_NXB == null) {
+				this.setTenNXB(newtenNXB);
+				break;
+			} else if (new_NXB.getTrangthai() == 1)
+				System.out.println("Trung lap NXB - hay nhap lai:");
+			else {
+				System.out.println("NXB da bi xoa du lieu - hay lua chon:");
+				System.out.println("1.Khoi phuc thong tin NXB ");
+				System.out.println("2.Khoi phuc va Sua thong tin NXB");
+				System.out.println("3.Tao NXB moi");
+				
+				int luachon_1 = Integer.parseInt(sc.nextLine());
+				
+				switch (luachon_1) {
+				case 1:
+					danhsachNXB.Khoiphuc(newtenNXB);
+					System.out.println("Da khoi phuc");
+					return;
+				case 2:
+					danhsachNXB.Khoiphuc(newtenNXB);
+					danhsachNXB.Sua(newtenNXB);
+					System.out.println("Dang Sua");
+					return;
+				default:
+					danhsachNXB.Them();
+					return;
+				}
+			}
+		}
+		while (this.getsdtNXB() == null || this.getsdtNXB().isEmpty()) {
+			System.out.print("Nhap So dien thoai NXB: ");
+			String newsdtNXB = sc.nextLine();
+			
+			if (newsdtNXB.matches("\\d+"))
+				this.setsdtNXB(newsdtNXB);
+			else
+				this.setsdtNXB(null);
+			
+		}
+		
+		while (this.getDcNXB() == null || this.getDcNXB().isEmpty()) {
+			System.out.print("Nhap Dia chi NXB: ");
+			this.setDcNXB(sc.nextLine());
+		}
+		
+		this.trangthai = 1;
+	}
+@Override
+	public void suathongtin() {
+		System.out.println("1.Sua ten NXB");
+		System.out.println("2.Sua So dien thoai NBX");
+		System.out.println("3.Sua Dia chi NXB");
+		System.out.print("Nhap lua chon: ");
+		
+		int luachon = Integer.parseInt(sc.nextLine());
+		boolean exitLoop = false;
+		
+		switch (luachon) {
+		case 1:
+			while (!exitLoop) {
+				System.out.println("Nhap ten NXB moi");
+				String newtenNXB = sc.nextLine();
+				
+				NXB new_NXB = danhsachNXB.Trunglap(newtenNXB);
+				
+				if (new_NXB == null && !this.getTenNXB().equals(newtenNXB)) {
+					this.setTenNXB(newtenNXB);
+					exitLoop = true;
+					break;
+				} else if (new_NXB != null && new_NXB.getTrangthai() == 1) {
+					System.out.println("Trung lap NXB moi nhap lai:");
+					exitLoop = false;
+				} else {
+					System.out.println("NXB da bi xoa du lieu - hay lua chon:");
+					System.out.println("1.Khoi phuc NXB");
+					System.out.println("2.Sua va khoi phuc thong tin NXB");
+					System.out.println("3.Thoat");
+					
+					int luachon_1 = Integer.parseInt(sc.nextLine());
+					
+					switch (luachon_1) {
+					case 1:
+						danhsachNXB.Khoiphuc(newtenNXB);
+						System.out.println("Da khoi phuc");
+						return;
+					case 2:
+						danhsachNXB.Khoiphuc(newtenNXB);
+						danhsachNXB.Sua(newtenNXB);
+						System.out.println("Dang Sua");
+						return;
+					default:
+						return;
+					}
+				}
+				
+			}
+			break;
+		case 2:
+			while (!exitLoop) {
+				System.out.print("Nhap so dien thoai moi cua NXB: ");
+				String newsdtNXB = sc.nextLine();
+				
+				if (newsdtNXB.matches("\\d+") && !newsdtNXB.equals(this.getsdtNXB())) {
+					this.setsdtNXB(newsdtNXB);
+					exitLoop = true;
+				}
+				
+			}
+			break;
+		case 3:
+			while (!exitLoop) {
+				System.out.print("Nhap Dia chi moi cua NXB: ");
+				String newdcNXB = sc.nextLine();
+				
+				if (!newdcNXB.equals(this.getDcNXB())) {
+					this.setDcNXB(newdcNXB);
+					exitLoop = true;
+				}
+				
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void suathongtin_chopheptrunglap() {
+		System.out.println("1.Sua ten NXB");
+		System.out.println("2.Sua So dien thoai NBX");
+		System.out.println("3.Sua Dia chi NXB");
+		System.out.print("Nhap lua chon: ");
+		
+		int luachon = Integer.parseInt(sc.nextLine());
+		boolean exitLoop = false;
+		
+		switch (luachon) {
+		case 1:
+			while (!exitLoop) {
+				NXB new_tenNXB = new NXB();
+				System.out.print("Nhap Ten moi cua NXB: ");
+				String newtenNXB = sc.nextLine();
+				
+				new_tenNXB = danhsachNXB.Trunglap(newtenNXB);
+				
+				if (!exitLoop && this.getTenNXB() != null && !newtenNXB.equals(this.getTenNXB())) {
+					if (new_tenNXB == null) {
+						this.setTenNXB(newtenNXB);
+						exitLoop = true;
+						break;
+					} else {
+						if (new_tenNXB.getTrangthai() == 1) {
+							System.out.println("Trung lap NXB - hay lua chon:");
+							System.out.println("1.Nhan thon tin NXB ");
+							System.out.println("2.Nhan va Sua thong tin NXB");
+							System.out.println("3.Thoat");
+							
+							int luachon_1 = Integer.parseInt(sc.nextLine());
+							
+							switch (luachon_1) {
+							case 1:
+								System.out.println("Da khoi phuc");
+								break;
+							case 2:
+								new_tenNXB = danhsachNXB.Sua(newtenNXB);
+								System.out.println("Dang Sua");
+								break;
+							default:
+								return;
+							}
+							
+						} else {
+							System.out.println("NXB da bi xoa du lieu - hay lua chon:");
+							System.out.println("1.Khoi phuc va Nhan thong tin NXB ");
+							System.out.println("2.Khoi phuc va Sua thong tin NXB");
+							System.out.println("3.Thoat");
+							
+							int luachon_1 = Integer.parseInt(sc.nextLine());
+							
+							switch (luachon_1) {
+							case 1:
+								new_tenNXB = danhsachNXB.Khoiphuc(newtenNXB);
+								System.out.println("Da khoi phuc");
+								break;
+							case 2:
+								danhsachNXB.Khoiphuc(newtenNXB);
+								new_tenNXB = danhsachNXB.Sua(newtenNXB);
+								System.out.println("Dang Sua");
+								break;
+							default:
+								return;
+							}
+						}
+					}
+				}
+				
+				if (new_tenNXB != null) {
+					capnhatNXB(new_tenNXB.getTenNXB(), new_tenNXB.getsdtNXB(), new_tenNXB.getDcNXB(), 1);
+					exitLoop = true;
+					return;
+				}
+				
+			}
+			break;
+		case 2:
+			while (!exitLoop) {
+				System.out.print("Nhap so dien thoai moi cua NXB: ");
+				String newsdtNXB = sc.nextLine();
+				
+				if (newsdtNXB.matches("\\d+") && !newsdtNXB.equals(this.getsdtNXB())) {
+					this.setsdtNXB(newsdtNXB);
+					exitLoop = true;
+				}
+				
+			}
+			break;
+		case 3:
+			while (!exitLoop) {
+				System.out.print("Nhap Dia chi moi cua NXB: ");
+				String newdcNXB = sc.nextLine();
+				
+				if (!newdcNXB.equals(this.getDcNXB())) {
+					this.setDcNXB(newdcNXB);
+					exitLoop = true;
+				}
+				
+			}
+			break;
+		default:
+			break;
+		}
+	}
+}
